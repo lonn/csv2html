e6b12e18343f1b8ab496e375bfec98c4b0eaeff9
lonn <lonnglau@gmail.com>
2013-04-15 21:52:38 +0800

    Update csv2html.py

--- csv2html.py
+++ csv2html.py
@@ -4,16 +4,21 @@ import csv
 script, filename = argv
 table = ""
 
+#open .csv file
 with open(filename, 'rb') as csvfile:
 	reader = csv.reader(csvfile)
-
+	
+	#generate table
 	table += "<table border='1'>"
 
+	#get every row in .csv
 	for row in reader:
+		#get every item in row
 		table += "<tr>\n" + "".join(["<td>%s</td>\n" % item for item in row]) + "</tr>\n"
 
 	table += "</table>"
 
+#write to .html
 tofile = filename + ".html"
 with open(tofile, 'w+') as htmlfile:
 	htmlfile.write(table)
